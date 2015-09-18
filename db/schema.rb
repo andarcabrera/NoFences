51e393bb32a4835d741ae2e561bebd03a79fe2d3
Alex Green <email": null>
2015-09-18 13:43:12 -0500

    Add user and categroy seeds

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20150917212231) do
     t.string   "first_name",      null: false
     t.string   "last_name",       null: false
     t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
