544768fe0c51801dd646890648e696731e31f4e0
Cat McLoughlin & Meg Tuls <pair+catmclough.meglkts@devbootcamp.com>
2015-09-18 15:26:12 -0500

    Add a bare registration and login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def create
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      # flash[:success] = "Welcome to NoFences, #{@user.first_name}!"
+      redirect_to categories_path
+    else
+      render "new"
+      # append errors' full messages
+    end
   end
 
   def update
@@ -15,5 +25,11 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password )
   end
 end
