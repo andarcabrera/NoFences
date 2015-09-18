9c6e3b098e10eac6a07fc67f05c7b2bb2ccfaf3a
Meg Tuls <meglktuls@gmail.com>
2015-09-18 17:20:36 -0500

    Add skeleton for category show page with minor css

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :categorized_organizations
   has_many :organizations, through: :categorized_organizations
+  has_many :posts
 
   validates_presence_of :name
 end
