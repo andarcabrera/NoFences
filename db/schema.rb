6c8007815ff111e69c44da6ab548da8d55442cff
Alex Green <email": null>
2015-09-19 15:35:16 -0500

    add image div

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20150919004618) do
     t.string   "first_name",      null: false
     t.string   "last_name",       null: false
     t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "provider"
