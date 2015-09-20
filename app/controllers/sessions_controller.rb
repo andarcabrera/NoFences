a92d7d6d9e04657ab22e87163645889c0a4c1a0d
Alex Green <email": null>
2015-09-19 21:54:55 -0500

    Add ajax to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,51 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:session]
-      user = User.find_by(email: params[:session][:email])
+    # if params[:session]
+    #   user = User.find_by(email: params[:session][:email])
+    #   if user && user.authenticate(params[:session][:password])
+    #     log_in(user)
+    #     redirect_to root_url
+    #   else
+    #     flash[:unsuccessful] = "Your email or password were entered incorrectly."
+    #   end
+    # else
+    #   auth = request.env["omniauth.auth"]
+    #   user = User.where(:provider => auth['provider'],
+    #                   :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    #   session[:user_id] = user.id
+    #   redirect_to root_url, :notice => "Signed in!"
+    # end
+
+    if params[:session] == nil
+      auth = request.env["omniauth.auth"]
+       user = User.where(:provider => auth['provider'],
+                       :uid => auth['uid']).first || User.create_with_omniauth(auth)
+       session[:user_id] = user.id
+       redirect_to root_url, :notice => "Signed in!"
+  else
+    user = User.find_by(email: params[:session][:email])
+    if request.xhr?
+
+      if user && user.authenticate(params[:session][:password])
+        log_in(user)
+        p "success" * 100
+        redirect_to root_url
+      else
+        p "failure" * 100
+      end
+    else
       if user && user.authenticate(params[:session][:password])
         log_in(user)
         redirect_to root_url
       else
+        redirect_to root_url
         flash[:unsuccessful] = "Your email or password were entered incorrectly."
       end
-    else
-      auth = request.env["omniauth.auth"]
-      user = User.where(:provider => auth['provider'],
-                      :uid => auth['uid']).first || User.create_with_omniauth(auth)
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Signed in!"
     end
+  end
+
+
   end
 
   def destroy
