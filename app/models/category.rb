39ebe3e130a3e270d829ddb603338c5e7b5a3186
Meg Tuls <meglktuls@gmail.com>
2015-09-20 14:58:57 -0500

    Move services offered and wanted methods to category to call by category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ActiveRecord::Base
   has_many :posts
 
   validates_presence_of :name
+
+  def services_offered
+  	self.posts.where(volunteer: true)
+  end
+
+  def services_wanted
+  	self.posts.where(volunteer: nil)
+  end 
 end
