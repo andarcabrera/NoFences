b789620a55e4c7299cd783b6c81ec6391863bed0
Anda Cabrera <andarcabrera@gmail.com>
2015-09-20 22:52:32 -0500

    Add functioning amazon s3 storage for uploaded photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,11 @@
+
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:               'AWS',
-    aws_access_key_id:      'AKIAIMVRLWZBMWOYM43Q',
-    aws_secret_access_key:  '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',
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
-
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage           = :file
-    config.enable_processing = false
-    config.root              = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-
-  config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
-  config.fog_directory    = 'devbootcampnofences'
+  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
