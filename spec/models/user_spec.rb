c40f7c1ff477c02b3cb3556096ee046012132367
Alex Green <email": null>
2015-09-23 10:07:16 -0500

    precompile

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
 describe User do
+  let (:user) { User.new(first_name: "Alex", last_name: "Blue", email: "alex@aol.com", password: "bop") }
+
 
 
 #METHOD TESTS WILL LIVE HERE!
 
+  describe "#full_name" do
+    it "returns the users full name" do
+      expect(user.full_name).to eq("Alex Blue")
+    end
+  end
 
+  describe "#display_languages" do
+    user2 = User.create!(first_name: "Alex", last_name: "Blue", email: "alex@aol.com", password: "bop")
+    english = Language.new(name: "English")
+    spanish = Language.new(name: "Spanish")
+    p user2.languages
+    user2.languages << english
+    user2.languages << spanish
+    p user2.languages
+    it "display the users languages in an easy to ready way" do
+      expect(user2.display_languages).to eq("English, Spanish")
+    end
+  end
 
 
 
@@ -23,11 +42,11 @@ describe User do
     expect(user.errors[:last_name]).to_not be_empty
   end
 
-  it "is NOT valid without an email" do
-    user = User.new(first_name: "kyle", last_name: "heehaw")
-    user.valid?
-    expect(user.errors[:email]).to_not be_empty
-  end
+  # it "is NOT valid without an email" do
+  #   user = User.new(first_name: "kyle", last_name: "heehaw")
+  #   user.valid?
+  #   expect(user.errors[:email]).to_not be_empty
+  # end
 
   it "is NOT valid without an password or uid" do
     user = User.new(first_name: "kyle", last_name: "heehaw", email: "haha@b.com")
@@ -47,8 +66,6 @@ describe User do
     user.valid?
     expect(user.errors).to be_empty
   end
-
-
 
  end
 
